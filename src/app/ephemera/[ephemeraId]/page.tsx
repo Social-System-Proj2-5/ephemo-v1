ad0ba524abc77857cd7e6dd9df4d8506020b44b4
Paulliant <paulliantchen@gmail.com>
2026-07-14 14:32:47 +0900

    ephemera frontend

--- src/app/ephemera/[ephemeraId]/page.tsx
+++ src/app/ephemera/[ephemeraId]/page.tsx
@@ -136,10 +136,10 @@ export default function EphemeraDetailPage() {
             </h1>
           </div>
           <Link
-            href="/ephemera"
+            href="/"
             className="rounded-md border border-stone-300 bg-white px-4 py-2 text-sm font-medium transition hover:bg-stone-100"
           >
-            一覧へ戻る
+            ホーム
           </Link>
         </header>
 
