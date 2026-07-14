1131d2f2c7c5345bdc312bd1f053034e28d8bdf5
Paulliant <paulliantchen@gmail.com>
2026-07-14 15:17:57 +0900

    local host allowed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "10.*.*.*",
+    "172.*.*.*",
+    "192.168.*.*",
+    "169.254.*.*",
+  ],
 };
 
 export default nextConfig;
